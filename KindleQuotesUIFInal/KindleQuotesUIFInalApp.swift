8f640bdd4aec44346fb1022082295f9e173b6445
Pranit Duddupudi <pranitd23@gmail.com>
2024-08-03 21:38:43 -0400

    Initial Commit

---
+++ KindleQuotesUIFInal/KindleQuotesUIFInalApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindleQuotesUIFInalApp.swift
+//  KindleQuotesUIFInal
+//
+//  Created by Pranit Duddupudi on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct KindleQuotesUIFInalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
